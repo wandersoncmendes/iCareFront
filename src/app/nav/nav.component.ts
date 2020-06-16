c6f61d655377cba8b6d5cf93ce71b5fd5243fd67
Wanderson <wmendes99@gmail.com>
2020-06-15 22:46:58 -0300

    first commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  sidenavWidth = 4;
+  // opened = false;
+  isExpanded = false;
+
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  miniVariant(){
+    if (this.isExpanded) {
+      this.sidenavWidth = 4;
+    } else {
+      this.sidenavWidth = 15;
+    }
+    this.isExpanded = !this.isExpanded
+    console.log('increase sidenav width');
+  }
+
+}
