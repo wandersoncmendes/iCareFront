b34b1e0204b96fc5ebbdfa0ecf4c34461bd902b9
Wanderson <wmendes99@gmail.com>
2020-06-17 02:39:40 -0300

    scrollbar

--- src/app/core/breadcrumb/breadcrumb.module.ts
+++ src/app/core/breadcrumb/breadcrumb.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -8,7 +9,8 @@ import { BreadcrumbComponent } from './breadcrumb.component';
   declarations: [BreadcrumbComponent],
   exports: [BreadcrumbComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class BreadcrumbModule { }
